7654e86c19b89428f2edf8a77e80cce8be04cd37
lukas681 <lukas681@gmail.com>
2017-03-12 19:16:22 +0100

    Updating the map now works correctly

--- JS/googleChart.js
+++ JS/googleChart.js
@@ -1,16 +1,25 @@
 /**
  * Created by Lukas on 12.03.2017.
  */
-function drawRegionsMap(dataN) {
 
-    console.log(data);
-    var data = google.visualization.arrayToDataTable(dataN);
+/**
+ * After the initial drawRegionsMap we can simply change code by
+ * @param data
+ */
+ function updateChart(data){
+    var data = google.visualization.arrayToDataTable(data);
     var options = {};
     var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
     chart.draw(data, options);
-    console.log("finished");
+ }
+
+/**
+ * Sets the default values for our world map
+ */
+function drawRegionsMap() {
+    var datas = [
+        ['Country', 'Popularity'],
+        ['Germany', 100]];
+    updateChart(datas);
 }
-function loadGoogle(){
-    google.charts.load('current', {'packages': ['geochart']});
-    google.charts.setOnLoadCallback(drawRegionsMap);
-}
+
