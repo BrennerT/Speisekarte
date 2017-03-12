f7dd9a082df76941f84521112b55042aa96da8f1
lukas681 <lukas681@gmail.com>
2017-03-12 18:24:42 +0100

    Improvements

--- JS/googleChart.js
+++ JS/googleChart.js
@@ -1,11 +1,16 @@
 /**
  * Created by Lukas on 12.03.2017.
  */
-function drawRegionsMap(data) {
-    alert("Script start");
+function drawRegionsMap(dataN) {
+
     console.log(data);
-    var data = google.visualization.arrayToDataTable(data);
+    var data = google.visualization.arrayToDataTable(dataN);
     var options = {};
     var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
     chart.draw(data, options);
+    console.log("finished");
+}
+function loadGoogle(){
+    google.charts.load('current', {'packages': ['geochart']});
+    google.charts.setOnLoadCallback(drawRegionsMap);
 }
