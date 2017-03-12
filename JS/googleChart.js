7bb73352e5aaca62bd721b83290e002634c59b11
lukas681 <lukas681@gmail.com>
2017-03-12 17:59:11 +0100

    Improved Google API

--- JS/googleChart.js
+++ JS/googleChart.js
@@ -1,25 +1,11 @@
 /**
  * Created by Lukas on 12.03.2017.
  */
-
-google.charts.load('current', {'packages':['geochart']});
-google.charts.setOnLoadCallback(drawRegionsMap);
-
-function drawRegionsMap() {
-
-    var data = google.visualization.arrayToDataTable([
-        ['Country', 'Popularity'],
-        ['Germany', 200],
-        ['United States', 300],
-        ['Brazil', 400],
-        ['Canada', 500],
-        ['France', 600],
-        ['RU', 700]
-    ]);
-
+function drawRegionsMap(data) {
+    alert("Script start");
+    console.log(data);
+    var data = google.visualization.arrayToDataTable(data);
     var options = {};
-
     var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-
     chart.draw(data, options);
 }
