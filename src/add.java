26121e58c53e9335c886f6e75e81690f1dc3e700
BrennerT <torben.brenner@googlemail.com>
2017-03-27 14:37:23 +0200

    added post method in add Servlet

--- src/add.java
+++ src/add.java
@@ -2,11 +2,14 @@
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Database.DatabaseConnection;
 
 /**
  * Servlet implementation class add
@@ -27,8 +30,7 @@ public class add extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// do nothing
 	}
 
 	/**
@@ -37,7 +39,25 @@ public class add extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		doGet(request, response);
+		// get data from client
+		//try{
+			String name = request.getParameter("name");
+			String countries = request.getParameter("countries");
+			int count = Integer.parseInt(request.getParameter("count"));
+			String explanation = request.getParameter("explanation");
+			String wikipediaUrl = request.getParameter("wikipedia");
+		//}catch(NumberFormatException nfe){
+			//nfe.printStackTrace();		
+		//}
+						
+			System.out.println("This works then this message is displayed");
+				
+			DatabaseConnection dbCon = new DatabaseConnection();
+			dbCon.initConnection();
+						
+			RequestDispatcher rd = request.getRequestDispatcher("/HTML/speisekarteFormular.html");
+			rd.forward(request, response);
+				
 	}
 
 }
